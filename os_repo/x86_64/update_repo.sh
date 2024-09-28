71b49979be4fa8fed94ef82ceaf26d9b1e2c59f6
Tomasz Wyderka <tomasz.wyderka@ing.pl>
2024-09-28 19:46:27 +0200

    Rebuild repo

--- os_repo/x86_64/update_repo.sh
+++ os_repo/x86_64/update_repo.sh
@@ -4,7 +4,10 @@ rm otterlinux_repo*
 
 echo "add-repo"
 
-repo-add -s -n -R otterlinux_repo.db.tar.gz *.pkg.tar.zst
+#disable -s 
+#repo-add -s -n -R otterlinux_repo.db.tar.gz *.pkg.tar.zst
+
+repo-add -n -R otterlinux_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 
 echo "Repo updated!"
